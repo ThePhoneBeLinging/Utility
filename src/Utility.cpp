fe1c85dff49227d5434b2a2112e650c846cef387
Elias Aggergaard Larsen <s235125@dtu.dk>
2024-11-21 02:20:04 +0100

    Added read-write

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -6,6 +6,7 @@
 
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
 
 bool Utility::mRandInitialised = false;
@@ -23,4 +24,27 @@ int Utility::getRandomInt(int min, int max)
         returnValue = rand() % max;
     }
     return returnValue;
+}
+
+void Utility::writeToFile(const std::string& fileName, const std::string& msg)
+{
+    std::ofstream MyFile(fileName);
+    MyFile << msg;
+    MyFile.close();
+}
+
+std::string Utility::readFromFile(std::string fileName)
+{
+    // Create a text string, which is used to output the text file
+    std::string myText;
+
+    // Read from the text file
+    std::ifstream MyReadFile(fileName);
+    std::string returnString;
+    // Use a while loop together with the getline() function to read the file line by line
+    while (getline (MyReadFile, myText)) {
+        returnString.append(myText);
+    }
+    MyReadFile.close();
+    return returnString;
 }
--- src/include/Utility/Utility.h
+++ src/include/Utility/Utility.h
@@ -4,12 +4,14 @@
 
 #ifndef UTILITY_H
 #define UTILITY_H
-
+#include <string>
 
 
 class Utility {
 public:
     static int getRandomInt(int min, int max);
+    static void writeToFile(const std::string& fileName, const std::string& msg);
+    static std::string readFromFile(std::string fileName);
 private:
     static bool mRandInitialised;
 };
