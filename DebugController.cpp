163a53a8ec7becdcc151448741331ce80ea48ec8
EAL <elias@takefive.dk>
2025-03-02 21:15:05 +0100

    Implemented DebugController

---
+++ DebugController.cpp
@@ -0,0 +1,41 @@
+//
+// Created by eal on 3/2/25.
+//
+
+#include "DebugController.h"
+
+#include <iostream>
+
+#include "Utility/ConfigController.h"
+#include "Utility/Utility.h"
+
+void DebugController::initDebug(const int limit)
+{
+    currentIndexInt_ = 0;
+    debugLines_.resize(limit);
+}
+
+void DebugController::debugWrite(const std::string& debugText)
+{
+    std::lock_guard<std::mutex> lock(debugMutex_);
+    debugLines_[currentIndexInt_] = debugText;
+    currentIndexInt_++;
+    currentIndexInt_ %= debugLimit_;
+}
+
+void DebugController::dumpDebug(const std::string* src)
+{
+    std::lock_guard<std::mutex> lock(debugMutex_);
+    for (const auto& line : debugLines_)
+    {
+        if (src != nullptr)
+        {
+            Utility::appendToFile(*src,line);
+            Utility::appendToFile(*src,"\n");
+        }
+        else
+        {
+            std::cout << line << std::endl;
+        }
+    }
+}
---
+++ DebugController.h
@@ -0,0 +1,28 @@
+//
+// Created by eal on 3/2/25.
+//
+
+#ifndef DEBUGCONTROLLER_H
+#define DEBUGCONTROLLER_H
+#include <mutex>
+#include <string>
+#include <vector>
+
+
+class DebugController
+{
+public:
+    static void initDebug(int limit);
+    static void debugWrite(const std::string& debugText);
+    // Give src = nullptr for simply printing to console.
+    static void dumpDebug(const std::string* src);
+private:
+    static inline int currentIndexInt_;
+    static std::mutex debugMutex_;
+    static inline std::vector<std::string> debugLines_;
+    static inline int debugLimit_;
+};
+
+
+
+#endif //DEBUGCONTROLLER_H
